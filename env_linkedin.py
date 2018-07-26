af53e6d63f7c9bff87b2665ad6c93f3bac112e33
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-07-26 10:49:36 +0000

    oauth tokens

--- env_linkedin.py
+++ env_linkedin.py
@@ -1,7 +1,7 @@
 import os
 
 
-os.environ.setdefault("client_id", "77h95pg6sa22zc")
-os.environ.setdefault("client_secret", "KmReTx8BSgTRmcsj")
-os.environ.setdefault("USER_TOKEN", "972493075317776385-F6SjPPjJgRs2tc5ktmFRkWiNBBHaAPh")
-os.environ.setdefault("USER_SECRET", "03zatnZGuWrMzpsxZAngfWNjoYtpT9OW1TqPBMk7IbBHD")
+os.environ.setdefault("consumer_key", "77h95pg6sa22zc")
+os.environ.setdefault("consumer_secret", "KmReTx8BSgTRmcsj")
+os.environ.setdefault("USER_TOKEN", "fa45ab98-d7d3-48ce-92af-c856dbcb0941")
+os.environ.setdefault("USER_SECRET", "13d20cb8-d517-4419-91e1-22c13fe1e732")
