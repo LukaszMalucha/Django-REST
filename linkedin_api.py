fd577e4eb2df0dfdf02f25ae0cca0e3a2dd51187
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-07-26 17:07:48 +0000

    loop design

--- linkedin_api.py
+++ linkedin_api.py
@@ -26,15 +26,26 @@ application = linkedin.LinkedInApplication(authentication)
 # g = application.get_profile()
 # print (g)
 
-
-### COMPANIES
-
-companies = application.search_company(selectors=[{'companies': ['name', 'universal-name', 'website-url', 'specialties']}], params={'keywords': 'Cork education', 'count': 20, 'start': 120})
-# Search URL is https://api.linkedin.com/v1/company-search:(companies:(name,universal-name,website-url))?keywords=apple%20microsoft
-
-print(companies)
-
 ### PEOPLE (FORBIDDEN - fill the form "https://www.linkedin.com/help/linkedin/ask/API-DVR")
 
 # people = application.search_profile(selectors=[{'people': ['first-name', 'last-name']}], params={'keywords': 'Sgourdas'})
 # print(people)
+
+
+### COMPANIES DICTIONARY
+
+companies = application.search_company(selectors=[{'companies': ['name', 'universal-name', 'website-url', 'specialties']}], params={'keywords': 'Cork education', 'count': 20, 'start': 0})
+companies2 = application.search_company(selectors=[{'companies': ['name', 'universal-name', 'website-url', 'specialties']}], params={'keywords': 'Cork education', 'count': 20, 'start': 20})
+
+
+# print(companies)
+element = 0
+while True:
+        comps = application.search_company(selectors=[{'companies': ['name', 'universal-name', 'website-url', 'specialties']}], params={'keywords': 'Dublin', 'count': 20, 'start': element})
+        comps['companies']['_start'] < comps['companies']['_total']
+        element += 20
+        print (comps['companies']['_start'])
+        print (comps['companies']['_total'])
+
+# with open('companies%s.json' % (params['start']), 'w') as outfile:
+#             json.dump(response.json(), outfile, indent=4)    
