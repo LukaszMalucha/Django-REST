87c42e63740f19f8472b4dc6d4733a9de447a75a
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-07-29 12:08:50 +0000

    it

--- linkedin_api.py
+++ linkedin_api.py
@@ -35,13 +35,17 @@ def collect_pages(m, count):
 ### IRELAND = 'facet' : ['location,ie:0']
 
 # FINANCE
+# companies = application.search_company(selectors=[{'companies': ['name']}], 
+#                                                                  params={ 'facet' : ['location,ie:0','industry,47,41,129,43,42,44,45,46,128,106'] })
+                                                                 
+# IT
 companies = application.search_company(selectors=[{'companies': ['name']}], 
-                                                                 params={ 'facet' : ['location,ie:0','industry,47,41,129,43,42,44,45,46,128,106'] })
+                                                                 params={ 'facet' : ['location,ie:0','industry,118,109,3,4,5,6,140,84,96'] })                                                                 
                                                                  
 # with open('companies_Try.json', 'w') as outfile:
 #         json.dump(companies, outfile, indent=4)  
 
-# print(companies)        
+print(companies)        
 
 total = companies['companies']['_total']
 count = int(total / 20)
@@ -50,13 +54,13 @@ collect_pages(20, count)
 
 # print(pages)    
  
-companies_Cork = []    
+companies_IT = []    
 for element in pages:
     comps = application.search_company(selectors=[{'companies': ['name', 'website-url', 'specialties','company-type',
                                                                  'square-logo-url','industries','employee-count-range',
-                                                                 'founded-year','num-followers',]}],  params={'facet' : ['location,ie:0','industry,47,41,129,43,42,44,45,46,128,106'], 'count': 20, 'start': element})
-    companies_Cork.append(comps)
+                                                                 'founded-year','num-followers',]}],  params={'facet' : ['location,ie:0','industry,118,109,3,4,5,6,140,84,96'], 'count': 20, 'start': element})
+    companies_IT.append(comps)
         
         
-with open('companies_Fin.json', 'w') as outfile:
-        json.dump(companies_Cork, outfile, indent=4)        
+with open('companies_IT.json', 'w') as outfile:
+        json.dump(companies_IT, outfile, indent=4)        
