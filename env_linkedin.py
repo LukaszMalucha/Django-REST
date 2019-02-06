71777614f639377c3d5907278c2fc49ee0b517b7
Lukasz_Malucha <lucasmalucha@gmail.com>
2019-02-06 11:10:50 +0000

    files

--- env_linkedin.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-os.environ.setdefault("CONSUMER_KEY", "77h95pg6sa22zc")
-os.environ.setdefault("CONSUMER_SECRET", "KmReTx8BSgTRmcsj")
-os.environ.setdefault("OAUTH_TOKEN", "fa45ab98-d7d3-48ce-92af-c856dbcb0941")
-os.environ.setdefault("OAUTH_TOKEN_SECRET", "13d20cb8-d517-4419-91e1-22c13fe1e732")
