d2ba3e5dbd3d3c95263fd1094ee8fbddb9d4c7e7
Lukasz Malucha <lucasmalucha@gmail.com>
2019-02-10 18:45:09 +0000

    serializers

--- languages/serializers.py
+++ languages/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from .models import Language
+from .models import Language, Paradigm, Programmer
+
+
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="languages:paradigm-detail")
+
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'name', 'url')
 
 
 class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="languages:language-detail")
+
     class Meta:
         model = Language
         fields = ('id', 'name', 'paradigm', 'url')
+
+
+class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="languages:programmer-detail")
+
+    class Meta:
+        model = Programmer
+        fields = ('id', 'url', 'name', 'languages')
