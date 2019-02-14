f79806f3ed4b6a24071cc93af84db20a9cabcb64
Lukasz Malucha <lucasmalucha@gmail.com>
2019-02-14 19:00:30 +0000

    tag

--- recipe/views.py
+++ recipe/views.py
@@ -7,8 +7,8 @@ from core.models import Tag
 from recipe import serializers
 
 
-# we need only ListModel view
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+# we need only ListModel view and Create view
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -20,5 +20,9 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
 
 
