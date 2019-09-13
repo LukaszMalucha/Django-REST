2c87d6dff67158bf22fe333c0e80212d271ff9a0
unknown <lucasmalucha@gmail.com>
2019-09-13 07:27:51 +0100

    recipes

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -63,3 +63,24 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Cake',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+
+
+
+
+
+
+
+
+
