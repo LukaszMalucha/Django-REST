2a5d86451be04c27aa195772db8fd4417588dcd5
Lukasz Malucha <lucasmalucha@gmail.com>
2020-05-11 15:28:35 +0100

    docker

--- _old/companies/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from companies import views
-from rest_framework.urlpatterns import format_suffix_patterns
-
-app_name = 'companies'
-
-urlpatterns = [
-    path('stocks/', views.StockListView.as_view(), name='stocks'),
-]
