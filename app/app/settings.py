5629889fcf9e072adeae18013dd11dd320a103dc
Lukasz Malucha <lucasmalucha@gmail.com>
2020-05-15 13:49:03 +0100

    s_key

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+if "SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+else:
+    SECRET_KEY = "TestDjango"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), '*', ]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
